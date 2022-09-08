05023d335eb8b3be5b2f7fd815e88f3c131fff86
Nate Edwards <nedwards@esha.com>
2022-09-08 14:39:53 -0700

    Changing port back to 1433

--- createdb.ps1
+++ createdb.ps1
@@ -1,4 +1,4 @@
-sqlcmd -S localhost:1439 -U sa -P P@ssw0rd -Q "use master;
+sqlcmd -U sa -P P@ssw0rd -Q "use master;
 IF DB_ID('nxtgendb') IS NOT NULL
 	drop database nxtgendb;
 create database nxtgendb"
