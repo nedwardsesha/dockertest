d43852973842783b26f28ea712894415a6e6f495
Nate Edwards <nedwards@esha.com>
2022-09-08 14:52:12 -0700

    Adding in port to sqlcmd script again

--- createdb.ps1
+++ createdb.ps1
@@ -1,4 +1,4 @@
-sqlcmd -U sa -P P@ssw0rd -Q "use master;
+sqlcmd -S localhost:1439/sql-server-db -U sa -P P@ssw0rd -Q "use master;
 IF DB_ID('nxtgendb') IS NOT NULL
 	drop database nxtgendb;
 create database nxtgendb"
